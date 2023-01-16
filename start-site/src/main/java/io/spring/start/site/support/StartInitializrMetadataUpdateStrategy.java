a842bc722805c81dd0cfe2a9930d8cb78041d6e8
restmad <restmad@outlook.com>
2023-01-16 21:50:26 +0800

    ujpdate alibaba libs

--- start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
+++ start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
@@ -16,16 +16,21 @@
 
 package io.spring.start.site.support;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import io.spring.initializr.generator.version.Version;
+import io.spring.initializr.generator.version.VersionParser;
 import io.spring.initializr.metadata.DefaultMetadataElement;
 import io.spring.initializr.web.support.InitializrMetadataUpdateStrategy;
 import io.spring.initializr.web.support.SaganInitializrMetadataUpdateStrategy;
-
+import jodd.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 /**
  * An {@link InitializrMetadataUpdateStrategy} that performs additional filtering of
@@ -33,23 +38,116 @@ import org.springframework.web.client.RestTemplate;
  *
  * @author Stephane Nicoll
  */
+@Slf4j
 public class StartInitializrMetadataUpdateStrategy extends SaganInitializrMetadataUpdateStrategy {
+
+	private static final Comparator<DefaultMetadataElement> VERSION_METADATA_ELEMENT_COMPARATOR = new VersionMetadataElementComparator();
+
+	private final ObjectMapper objectMapper;
 
 	public StartInitializrMetadataUpdateStrategy(RestTemplate restTemplate, ObjectMapper objectMapper) {
 		super(restTemplate, objectMapper);
+		this.objectMapper = objectMapper;
 	}
 
 	@Override
 	protected List<DefaultMetadataElement> fetchSpringBootVersions(String url) {
-//		List<DefaultMetadataElement> versions = super.fetchSpringBootVersions(url);
-//		return (versions != null) ? versions.stream().filter(this::isCompatibleVersion).collect(Collectors.toList())
-//				: null;
-		return null;
+		log.info("fetchSpringBootVersions: {}, changed to read from json file", url);
+		URL resource = this.getClass().getResource("/spring/spring-version.json");
+		if (resource == null) {
+			return new ArrayList<>();
+		}
+		String path = resource.getPath();
+		try {
+			String springVersionJson = FileUtil.readString(path);
+			JsonNode content = objectMapper.readTree(springVersionJson);
+			log.info("spring version content: {}", content.toPrettyString());
+			return getBootVersion(content);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	private boolean isCompatibleVersion(DefaultMetadataElement versionMetadata) {
-		Version version = Version.parse(versionMetadata.getId());
-		return (version.getMajor() == 2 && version.getMinor() > 6) || (version.getMajor() >= 3);
+	private List<DefaultMetadataElement> getBootVersion(JsonNode content) {
+		ArrayNode releases = (ArrayNode) content.get("projectReleases");
+		List<DefaultMetadataElement> list = new ArrayList<>();
+
+		for (JsonNode node : releases) {
+			DefaultMetadataElement versionMetadata = this.parseVersionMetadata(node);
+			if (versionMetadata != null) {
+				list.add(versionMetadata);
+			}
+		}
+
+		list.sort(VERSION_METADATA_ELEMENT_COMPARATOR.reversed());
+		return list;
+	}
+
+	private DefaultMetadataElement parseVersionMetadata(JsonNode node) {
+		String versionId = node.get("version").textValue();
+		Version version = VersionParser.DEFAULT.safeParse(versionId);
+		if (version == null) {
+			return null;
+		}
+		else {
+			DefaultMetadataElement versionMetadata = new DefaultMetadataElement();
+			versionMetadata.setId(versionId);
+			versionMetadata.setName(this.determineDisplayName(version));
+			versionMetadata.setDefault(node.get("current").booleanValue());
+			return versionMetadata;
+		}
+	}
+
+	private String determineDisplayName(Version version) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(version.getMajor()).append(".").append(version.getMinor()).append(".").append(version.getPatch());
+		if (version.getQualifier() != null) {
+			sb.append(this.determineSuffix(version.getQualifier()));
+		}
+
+		return sb.toString();
+	}
+
+	private static class VersionMetadataElementComparator implements Comparator<DefaultMetadataElement> {
+
+		private static final VersionParser versionParser;
+
+		private VersionMetadataElementComparator() {
+		}
+
+		public int compare(DefaultMetadataElement o1, DefaultMetadataElement o2) {
+			Version o1Version = versionParser.parse(o1.getId());
+			Version o2Version = versionParser.parse(o2.getId());
+			return o1Version.compareTo(o2Version);
+		}
+
+		static {
+			versionParser = VersionParser.DEFAULT;
+		}
+
+	}
+
+	private String determineSuffix(Version.Qualifier qualifier) {
+		String id = qualifier.getId();
+		if (id.equals("RELEASE")) {
+			return "";
+		}
+		else {
+			StringBuilder sb = new StringBuilder(" (");
+			if (id.contains("SNAPSHOT")) {
+				sb.append("SNAPSHOT");
+			}
+			else {
+				sb.append(id);
+				if (qualifier.getVersion() != null) {
+					sb.append(qualifier.getVersion());
+				}
+			}
+
+			sb.append(")");
+			return sb.toString();
+		}
 	}
 
 }
